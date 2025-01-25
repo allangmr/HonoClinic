db069751a5f46b60c0602c54022cbc42299462c1
Allan Montilla <allangmr7@gmail.com>
2025-01-25 14:21:22 -0500

    Add: Add types for the appwrite db

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -3,13 +3,13 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
-import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
 
 export enum FormFieldType {
   Input = "input",
@@ -39,9 +39,8 @@ const PatientForm = () => {
 
     try {
       const userData = { name, email, phone};
-      // const user = await createUser(userData);
-      // if(user) router.push(`/patients/${user.$id}/register`);
-      console.log(userData);
+      const user = await createUser(userData);
+      if(user) router.push(`/patients/${user.$id}/register`);
       
     } catch (error) {
       console.error(error);
