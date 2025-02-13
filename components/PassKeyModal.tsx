22ecb1a95e2726dfd222823623bf95e410ff0f9b
Allan Montilla <allangmr7@gmail.com>
2025-02-12 23:05:28 -0500

    Update: PassKeyModal layout

--- components/PassKeyModal.tsx
+++ components/PassKeyModal.tsx
@@ -1,6 +1,52 @@
+'use client'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+
 const PassKeyModal = () => {
+  const router = useRouter()
+  const [open, setOpen] = useState(true);
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/")
+  }
   return (
-    <div>PassKeyModal</div>
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin Access Verification
+            <Image
+              src="/assets/icons/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => closeModal()}
+              className="cursor-pointer"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            To access the admin dashboard, please enter the passkey.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel onClick={() => closeModal()}>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+
   )
 }
 
