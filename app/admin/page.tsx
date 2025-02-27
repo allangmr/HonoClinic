0e1db23feab579d9b0624c5b8a22280b9b2e6857
Allan Montilla <allangmr7@gmail.com>
2025-02-26 20:55:16 -0500

    feat: add header to admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Admin = () => {
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
-        <header className='flex flex-col items-center space-y-4'>
+        <header className='admin-header'>
             <Link href="/" className='cursor-pointer'>
                 <Image 
                     src="/assets/icons/logo-full.svg"
@@ -15,7 +15,16 @@ const Admin = () => {
                     className='h-8 w-fit-content'
                 />
             </Link>
+            <p className='text-16-semibold'>
+              Admin Dashboard
+            </p>
         </header>
+        <main className='admin-main'>
+          <section className='w-full space-y-4'>
+            <h1 className='header'>Welcome 👋</h1>
+            <p className='text-dark-700'>Start the day with managing new appointments</p>
+          </section>
+        </main>
     </div>
   )
 }
