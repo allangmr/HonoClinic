23f5916eaac2b52eb9fd8c444fb99cbf8d29a737
Allan Montilla <allangmr7@gmail.com>
2025-03-04 21:54:24 -0600

    feature: : Add appointment status count to recent appointments list.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,11 @@
 import StatCard from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Admin = () => {
+const Admin = async () => {
+  const appointments = await getRecentAppointmentList() || { scheduledCount: 0, pendingCount: 0, cancelledCount: 0 };
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
         <header className='admin-header'>
@@ -28,19 +30,19 @@ const Admin = () => {
           <section className='admin-stat'>
             <StatCard 
               type="appointments" 
-              count={5} 
+              count={appointments.scheduledCount} 
               label="Scheduled Appointments"
               icon="/assets/icons/appointments.svg"
             />
             <StatCard 
               type="pending" 
-              count={5} 
+              count={appointments.pendingCount} 
               label="Pending Appointments"
               icon="/assets/icons/pending.svg"
             />
             <StatCard 
               type="cancelled" 
-              count={5} 
+              count={appointments.cancelledCount} 
               label="Cancelled Appointments"
               icon="/assets/icons/cancelled.svg"
             />
