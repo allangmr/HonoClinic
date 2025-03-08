b109d87cccdb5afaf86f6008511b0a02a9d117cd
Allan Montilla <allangmr7@gmail.com>
2025-03-07 23:27:29 -0600

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,28 @@
+
+import {DataTable} from '@/components/table/DataTable'
 import StatCard from '@/components/StatCard'
 import { getRecentAppointmentList } from '@/lib/actions/appointment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import {columns, Payment} from '@/components/table/columns'
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
 
 const Admin = async () => {
-  const appointments = await getRecentAppointmentList() || { scheduledCount: 0, pendingCount: 0, cancelledCount: 0 };
+  const data = await getData();
+  const appointments = await getRecentAppointmentList() || { scheduledCount: 0, pendingCount: 0, cancelledCount: 0, documents: [] };
   return (
     <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
         <header className='admin-header'>
@@ -47,6 +64,8 @@ const Admin = async () => {
               icon="/assets/icons/cancelled.svg"
             />
           </section>
+          {/* <DataTable columns={columns} data={appointments.documents} /> */}
+          <DataTable columns={columns} data={data} />
         </main>
     </div>
   )
