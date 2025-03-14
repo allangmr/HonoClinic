ccfa6ceab669936664b54f75a73e08c4a1d12d27
Allan Montilla <allangmr7@gmail.com>
2025-03-13 23:13:38 -0600

    feat: Add text to StatusBadge component

--- components/StatusBadge.tsx
+++ components/StatusBadge.tsx
@@ -7,10 +7,17 @@ const StatusBadge = ({status}: {status: Status}) => {
   return (
     <div className={clsx('status-badge', {
       'bg-green-600' : status === 'scheduled',
-      'bg-yellow-600' : status === 'pending',
+      'bg-blue-600' : status === 'pending',
       'bg-red-600' : status === 'cancelled',
     })}>
       <Image src={StatusIcon[status]} alt={status} width={24} height={24} className='h-fit w-3' />
+      <p className={clsx('text-12-smibold capitalize', {
+        'text-green-500': status === 'scheduled',
+        'text-blue-500': status === 'pending',
+        'text-red-500': status === 'cancelled',
+      })}>
+        {status}
+      </p>
     </div>
   )
 }
