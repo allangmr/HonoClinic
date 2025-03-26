e8b94478ddc88d2aaada777501294ddbc0b58c70
Allan Montilla <allangmr7@gmail.com>
2025-03-26 13:08:47 -0600

    feat: Fix appointment reference issue

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -29,11 +29,11 @@ const AppointmentForm = ({userId, patientId, type, appointment, setOpen}: {
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      note: "",
-      cancellationReason: "",
+      primaryPhysician: appointment ? appointment.primaryPhysician : "",
+      schedule: appointment ? new Date(appointment.schedule) : new Date(),
+      reason: appointment ? appointment.reason : "",
+      note: appointment ? appointment.note : "",
+      cancellationReason: appointment ? appointment.cancellationReason : "",
     },
   })
 
@@ -115,10 +115,10 @@ const AppointmentForm = ({userId, patientId, type, appointment, setOpen}: {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
+        {type === 'create' && <section className="mb-12 space-y-4">
           <h1 className="header">New Appointment 🙌</h1>
           <p className="text-dark-700">Request a new appointment in 10 seconds</p>
-        </section>
+        </section>}
 
         { type !== "cancel" && (
             <>
