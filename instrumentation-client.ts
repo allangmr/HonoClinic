b7c2b7caf78a72ac69ed8746ed5f8071cda70621
Allan Montilla <allangmr7@gmail.com>
2025-03-31 22:09:53 -0600

    feat: Upgrade Sentry To Use Replay

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -7,6 +7,19 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://41eac930be133d0535b9226c47618a43@o4509062777929728.ingest.us.sentry.io/4509062780682240",
 
+   // Add optional integrations for additional features
+   integrations: [
+    Sentry.replayIntegration(),
+  ],
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while      
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
